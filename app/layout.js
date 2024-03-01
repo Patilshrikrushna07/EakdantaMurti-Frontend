0481ce4c0c31185b4319d708e15c1c9159dc4d76
Gaurav Madan <103850488+gauravmad@users.noreply.github.com>
2024-03-01 18:12:39 +0530

    navbr

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import "./globals.css";
+import "../utils/fontawesome";
 import { Navbar, Footer } from "../components";
 
 export const metadata = {
--- components/Navbar/index.js
+++ components/Navbar/index.js
@@ -1,22 +1,35 @@
-import React from 'react'
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Person2Icon from "@mui/icons-material/Person2";
+import SearchIcon from "@mui/icons-material/Search";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import ShoppingBagIcon from "@mui/icons-material/ShoppingBag";
 
 const Navbar = () => {
   return (
-    <div className="p-[2vh] bg-white shadow-xl ">
-      <img src="" alt="" />
-      <div>
-        <li>Home</li>
-        <li>About</li>
-        <li>Shop</li>
-        <li>Contact Us</li>
-      </div>
-      <div>
-        <li>Profile</li>
-        <li>Search</li>
-        <li></li>
+    <div className="bg-white shadow-md">
+      <div className="p-[2vh] w-[90%] mx-auto flex flex-row justify-between items-center">
+        <img src="/logo.jpg.png" className="w-[13vh]" alt="" />
+        <div className="list-none flex flex-row gap-x-[4vh]">
+          <li className="text-[2.5vh] font-semibold">Home</li>
+          <li className="text-[2.5vh] font-semibold">About</li>
+          <li className="text-[2.5vh] font-semibold">Shop</li>
+          <li className="text-[2.5vh] font-semibold">Contact Us</li>
+        </div>
+        <div className="list-none flex flex-row gap-x-[4vh]">
+          <Person2Icon className="cursor-pointer text-orange-800 text-[4.5vh]" />
+          <SearchIcon className="cursor-pointer text-orange-800 text-[4.5vh]" />
+          <FavoriteIcon className="cursor-pointer text-red-500 text-[4.5vh]" />
+          <div className="relative">
+            <p className="w-[4vh] h-[4vh] -top-[2vh] left-[2vh] bg-yellow-950 text-white rounded-full flex flex-row justify-center items-center absolute">
+              3
+            </p>
+            <ShoppingBagIcon className="cursor-pointer text-orange-300 text-[4.5vh]" />
+          </div>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Navbar;
