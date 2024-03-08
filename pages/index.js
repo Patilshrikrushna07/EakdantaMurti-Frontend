42a634f391e7064edd8dc344084a3c8da95498c3
Gaurav Madan <103850488+gauravmad@users.noreply.github.com>
2024-03-08 19:04:50 +0530

    fix issue reena

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import HeroBanner from '../components/HomeSection/HeroBanner';
-import Homesale from '@/components/HomeSection/Homesale';
+import Homesale from '../components/HomeSection/Homesale';
 
 
 export default function Home() {
