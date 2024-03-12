70dc8d773c6caa86df61d5b4f49a18197cf49567
Gaurav Madan <103850488+gauravmad@users.noreply.github.com>
2024-03-13 00:11:43 +0530

    navbar responsive

--- components/Navbar/index.js
+++ components/Navbar/index.js
@@ -4,30 +4,57 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Person2Icon from "@mui/icons-material/Person2";
 import SearchIcon from "@mui/icons-material/Search";
 import FavoriteIcon from "@mui/icons-material/Favorite";
+import MenuIcon from '@mui/icons-material/Menu';
+import CloseIcon from '@mui/icons-material/Close';
 import ShoppingBagIcon from "@mui/icons-material/ShoppingBag";
 import Link from "next/link";
 import { useStateContext } from "../../context/StateContext";
 import { toast } from "react-hot-toast";
 
 const Navbar = () => {
+  const { totalQuantities } = useStateContext();
+  const [mobileNavOpen, setMobileNavOpen] = useState(false);
 
-  const {totalQuantities}= useStateContext();
+  const toggleMobileNav = () => {
+    setMobileNavOpen(!mobileNavOpen);
+  };
 
   return (
     <div className="bg-white shadow-md mb-[1vh] static">
-      <div className="p-[2vh] w-[90%] mx-auto flex flex-row justify-between items-center">
-        <Link href="/"><img src="/favicon.ico" className="w-[13vh]" alt="" /></Link>
+      <div className="p-[2vh] w-[%] md:w-[90%] mx-auto flex flex-row justify-between items-center">
 
-        <div className="list-none flex flex-row gap-x-[4vh]">
-          <Link href="/"><li className="text-[2.5vh] font-semibold ">Home</li></Link>
-          <Link href="/about"><li className="text-[2.5vh] font-semibold">About</li></Link>
-          <Link href="/shop"><li className="text-[2.5vh] font-semibold">Shop</li></Link>
-          <Link href="/contact"><li className="text-[2.5vh] font-semibold">Contact</li></Link>
+        <div className="flex flex-row items-center">
+          <div
+            onClick={toggleMobileNav}
+            className="md:hidden mr-[2vh] cursor-pointer"
+          >
+            <MenuIcon className="text-[5vh] text-amber-950"/>
+          </div>
+
+          <Link href="/">
+            <img src="/favicon.ico" className="w-[13vh]" alt="" />
+          </Link> 
         </div>
-        <div className="list-none flex flex-row gap-x-[4vh]">
-          <Person2Icon className="cursor-pointer text-orange-800 text-[4.5vh]" />
-          <SearchIcon className="cursor-pointer text-orange-800 text-[4.5vh]" />
-          <FavoriteIcon className="cursor-pointer text-red-500 text-[4.5vh]" />
+        
+
+        <div className="navlinks list-none flex flex-row gap-x-[4vh]">
+          <Link href="/">
+            <li className="text-[2.7vh] font-semibold ">Home</li>
+          </Link>
+          <Link href="/about">
+            <li className="text-[2.7vh] font-semibold">About</li>
+          </Link>
+          <Link href="/shop">
+            <li className="text-[2.7vh] font-semibold">Shop</li>
+          </Link>
+          <Link href="/contact">
+            <li className="text-[2.7vh] font-semibold">Contact</li>
+          </Link>
+        </div>
+        <div className="list-none flex flex-row gap-x-[2vh] md:gap-x-[4vh]">
+          <Person2Icon className="cursor-pointer text-amber-800 text-[4.5vh]" />
+          <SearchIcon className="cursor-pointer text-amber-800 text-[4.5vh]" />
+          {/* <FavoriteIcon className="cursor-pointer text-red-500 text-[4.5vh]" /> */}
           <Link href="/checkout">
             <div className="relative">
               <p className="w-[4vh] h-[4vh] -top-[2vh] left-[2vh] bg-yellow-950 text-white rounded-full flex flex-row justify-center items-center absolute">
@@ -36,7 +63,43 @@ const Navbar = () => {
               <ShoppingBagIcon className="cursor-pointer text-orange-300 text-[4.5vh]" />
             </div>
           </Link>
-          
+        </div>
+
+        <div
+          className={`navlinks-mobile md:hidden fixed z-50 h-[100vh] w-[80%] top-0 bg-amber-100 flex flex-col justify-center items-center transition-transform duration-500 ease-in-out  ${
+            mobileNavOpen ? "-translate-x-[5vh]" : "-translate-x-[100vw]"
+          }`}
+        >
+          <div
+            onClick={toggleMobileNav}
+            className="absolute cursor-pointer top-0 right-0 p-[4vh]"
+          >
+            <CloseIcon
+              className="text-[5vh] text-amber"
+            />
+          </div>
+          <ul className="flex flex-col justify-center text-gray-700 items-center gap-x-[3vh]">
+            <Link href="/" onClick={toggleMobileNav}>
+              <li className="text-[3vh] my-[2vh] font-medium focus:underline">
+                Home
+              </li>
+            </Link>
+            <Link href="/aboutus" onClick={toggleMobileNav}>
+              <li className="text-[3vh] my-[2vh] font-medium">About</li>
+            </Link>
+            <Link href="/men" onClick={toggleMobileNav}>
+              <li className="text-[3vh] my-[2vh] font-medium">Men</li>
+            </Link>
+            <Link href="/women" onClick={toggleMobileNav}>
+              <li className="text-[3vh] my-[2vh] font-medium">Women</li>
+            </Link>
+            <Link href="/kids" onClick={toggleMobileNav}>
+              <li className="text-[3vh] my-[2vh] font-medium">Kids</li>
+            </Link>
+            <Link href="/contact" onClick={toggleMobileNav}>
+              <li className="text-[3vh] my-[2vh] font-medium">Contact</li>
+            </Link>
+          </ul>
         </div>
       </div>
     </div>
