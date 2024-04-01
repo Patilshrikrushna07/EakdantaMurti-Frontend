d2852e6ef5e012ec60adefdc1fe1146443012c24
Gaurav Madan <103850488+gauravmad@users.noreply.github.com>
2024-04-02 02:11:05 +0530

    fetch all order

--- components/AuthComponents/MyOrders.js
+++ components/AuthComponents/MyOrders.js
@@ -3,6 +3,7 @@ import { getCookie } from "cookies-next";
 import BackdropLoader from "../elements/BackdropLoader";
 import CircularProgress from "@mui/material/CircularProgress";
 import Link from "next/link";
+import { toast } from "react-hot-toast";
 
 const MyOrders = () => {
   const [ordersWithProductDetails, setOrdersWithProductDetails] = useState([]);
@@ -28,23 +29,30 @@ const MyOrders = () => {
         const ordersData = data.data;
         const ordersWithProductDetails = await Promise.all(
           ordersData.map(async (order) => {
-            const productResponse = await fetch(
-              `${apiUrl}/get-product/${order.items[0].product_id}`,
-              {
-                headers: {
-                  Authorization: `Bearer ${token}`,
-                },
-              }
+            const productDetails = await Promise.all(
+              order.items.map(async (item) => {
+                const productResponse = await fetch(
+                  `${apiUrl}/get-product/${item.product_id}`,
+                  {
+                    headers: {
+                      Authorization: `Bearer ${token}`,
+                    },
+                  }
+                );
+                if (productResponse.ok) {
+                  const productData = await productResponse.json();
+                  console.log("Product Fetched", productData);
+                  return { ...productData.data, quantity: item.quantity };
+                } else {
+                  console.error(
+                    `Failed to fetch product details for product ${item.product_id}`
+                  );
+                  return null;
+                }
+              })
             );
-            if (productResponse.ok) {
-              const productData = await productResponse.json();
-              return { ...order, product: productData.data }; // Add product details to order
-            } else {
-              console.error(
-                `Failed to fetch product details for order ${order._id}`
-              );
-              return order;
-            }
+
+            return { ...order, products: productDetails };
           })
         );
         setOrdersWithProductDetails(ordersWithProductDetails);
@@ -72,11 +80,18 @@ const MyOrders = () => {
     };
     const orderDate = new Date(dateString);
     const estimatedDeliveryDate = new Date(orderDate);
-    estimatedDeliveryDate.setDate(estimatedDeliveryDate.getDate() + 7); // Add 7 days
+    estimatedDeliveryDate.setDate(estimatedDeliveryDate.getDate() + 7);
     return {
       orderDate: orderDate.toLocaleString(undefined, options),
-      estimatedDeliveryDate: estimatedDeliveryDate.toLocaleString(undefined, options),
+      estimatedDeliveryDate: estimatedDeliveryDate.toLocaleString(
+        undefined,
+        options
+      ),
     };
+  };
+
+  const handleDownload = () => {
+    toast.error("Invoice not available!");
   };
 
   return (
@@ -90,23 +105,37 @@ const MyOrders = () => {
 
       ordersWithProductDetails.length > 0 ? (
         ordersWithProductDetails.map((order) => (
-          <div className="mb-[10vh] shadow-lg bg-[#fcfaf6] p-[2.5vh]" key={order._id}>
+          <div
+            className="mb-[10vh] shadow-lg bg-[#fcfaf6] p-[2.5vh]"
+            key={order._id}
+          >
             <div className="flex flex-row justify-between items-center">
               <h2 className="text-[3vh] font-semibold text-amber-950">
                 Order ID: <span className="font-normal"> #{order._id}</span>
               </h2>
               <div>
-                <button className="text-center px-[2vh] my-[2vh] font-semibold shadow-md hover:scale-110 hover:bg-[#B88E2F] hover:text-white transition-all mx-auto py-[1vh] text-[#B88E2F]  border-[#B88E2F] border-[0.3vh] mr-[2vh]">
+                <button
+                  className="text-center px-[2vh] my-[2vh] font-semibold shadow-md hover:scale-110 hover:bg-[#B88E2F] hover:text-white transition-all mx-auto py-[1vh] text-[#B88E2F]  border-[#B88E2F] border-[0.3vh] mr-[2vh]"
+                  onClick={handleDownload}
+                >
                   {" "}
                   Download Invoice
                 </button>
-                <button className="text-center px-[2vh] my-[2vh] font-semibold shadow-md hover:scale-110 bg-[#B88E2F] hover:bg-white text-white transition-all mx-auto py-[1vh] hover:text-[#B88E2F]  border-[#B88E2F] border-[0.3vh]">
+                <button
+                  className={`text-center px-[2vh] my-[2vh] font-semibold shadow-md hover:scale-110 transition-all mx-auto py-[1vh] border-[0.3vh] mr-[2vh] ${
+                    order.status === "Delivered"
+                      ? "bg-green-700 text-white hover:bg-green-700 border-green-700"
+                      : order.status === "Cancelled"
+                      ? "bg-red-700 text-white hover:bg-red-700 border-red-700"
+                      : "text-[#ffffff] border-[#ffa230] bg-[#ffa230] hover:text-white"
+                  }`}
+                >
                   {" "}
-                  Check Order Status
+                  {order.status}
                 </button>
               </div>
             </div>
-            <div className="flex flex-row justify-between items-center">
+            <div className="flex flex-row justify-between items-center mb-[5vh]">
               <p className="text-[3vh] font-semibold text-green-800">
                 Estimated Delivery:{" "}
                 <span className="font-normal">
@@ -115,22 +144,49 @@ const MyOrders = () => {
               </p>
               <p className="text-[3vh] font-semibold text-amber-950">
                 Order Date:{" "}
-                <span className="font-normal">      {formatDate(order.order_date).orderDate}
+                <span className="font-normal">
+                  {" "}
+                  {formatDate(order.order_date).orderDate}
                 </span>{" "}
               </p>
             </div>
 
-            <p>Status: {order.status}</p>
-            <p>Total Amount: {order.total_amount}</p>
-            <h3>Product Details:</h3>
-            <p>Name: {order.product.name}</p>
-            <p>Price: {order.product.price}</p>
-            <img
-              className="w-[40vh] bg-red-600 p-[1vh]"
-              src={order.product.images[0]}
-              alt={order.product.name}
-            />
-            {/* Render other product details as needed */}
+            {/* <p>Price: {order.product.price}</p> */}
+            {order.products.map((product, index) => (
+              <div
+                key={index}
+                className="flex flex-row w-[95%] mx-auto my-[1vh]"
+              >
+                <img
+                  className="w-[15vh] h-[18vh] object-cover mr-[4vh] p-[0.2vh] bg-[#111]"
+                  src={product.images[0]}
+                  alt={product.name}
+                />
+                <div className="flex flex-row justify-between">
+                  <div className="w-[55vw] ">
+                    <p className="text-[3vh] font-medium text-amber-950">
+                      {product.name}
+                    </p>
+                    <p className="text-[2.5vh] text-amber-950 my-[0.5vh]">
+                      {product.description}
+                    </p>
+                  </div>
+                  <div>
+                    <p className="text-[2.7vh] font-semibold text-amber-900">
+                      Qty:{" "}
+                      <span className="font-medium">{product.quantity}</span>{" "}
+                    </p>
+                    <p className="my-[1vh] text-[2.6vh]">
+                      MRP: Rs.{product.price}
+                    </p>
+                  </div>
+                </div>
+              </div>
+            ))}
+            <p className="text-[3vh] text-right font-medium mr-[5vh]">
+              Total Amount: &nbsp;&nbsp;
+              <span className="font-normal">Rs.{order.total_amount}</span>{" "}
+            </p>
           </div>
         ))
       ) : (
